dbd2b00f420b417b35e31edffdb5eda69ffe2da3
DrCausa <leo10said@gmail.com>
2025-09-04 22:12:28 -0500

    feat: add citas module with CRUD routes and controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const config = require('./config');
 
 const usuarios = require('./modulos/usuarios/rutas')
+const citas = require('./modulos/citas/rutas')
 const error = require('./red/errorres')
 
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 app.set('port', config.app.port)
 
 app.use('/api/usuarios', usuarios)
+app.use('/api/citas', citas)
 app.use(error);
 
 module.exports = app;
